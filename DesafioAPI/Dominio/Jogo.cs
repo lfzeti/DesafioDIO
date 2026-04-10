c75466c288bab29e3a3b0cf656311b43318f286e
lfzeti <157380989+lfzeti@users.noreply.github.com>
2026-04-10 15:56:43 -0300

    Create Jogo.cs

---
+++ DesafioAPI/Dominio/Jogo.cs
@@ -0,0 +1,9 @@
+namespace DesafioAPI.Dominio
+{
+    public class Jogo
+    {
+        public int Id {get; set;}
+        public string Nome {get; set;}
+        public bool Alugado {get; set;}
+    }
+}
